acd53b2aaaad7da6d8c170f60c7211b1639905ba
itsmealessandro <itsmealessandro@outlook.com>
2023-12-23 18:31:23 +0100

    query base

--- phpwebapp/configurazione.php
+++ phpwebapp/configurazione.php
@@ -1,13 +1,31 @@
 <?php
-/* Connettiamo il DB */
-$mysqli = new mysqli("localhost","test","SQLpass","dbtest");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-if($mysqli -> connect_errno){
-    printf("Connessione fallita: %s\n", $mysqli -> connect_error);
+/* Connettiamo il DB */
+$mysqli = new mysqli("localhost", "test", "SQLpass", "dbtest");
+
+if ($mysqli->connect_errno) {
+    printf("Connessione fallita: %s\n", $mysqli->connect_error);
     exit();
 }
-else{
-    printf("Connessione Riuscita");
+
+// Eseguire la query per ottenere l'utente con ID 1
+$query = "SELECT * FROM user WHERE ID = 1";
+$result = $mysqli->query($query);
+
+if (!$result) {
+    printf("Errore nella query: %s\n", $mysqli->error);
     exit();
 }
+
+// Ottenere i dati dell'utente
+$user = $result->fetch_assoc();
+
+// Output dei dati con printf
+printf("ID: %d, username: %s, password: %s", $user['ID'], $user['username'], $user['password']);
+
+// Chiudere la connessione al database
+$mysqli->close();
+exit();
 ?>
