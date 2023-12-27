8fba5f5d6fd129bdbd63df7c6c9e941ea6e94453
itsmealessandro <itsmealessandro@outlook.com>
2023-12-27 16:33:18 +0100

    aggiunto un template e modificata leggermente la struttura

--- config/configurazione.php
+++ config/configurazione.php
@@ -1,6 +1,8 @@
 <?php
+// Solo per Debugging: da rimuovere alla
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
 
 /* Connettiamo il DB */
 $mysqli = new mysqli("localhost", "test", "SQLpass", "dbtest");
@@ -10,22 +12,5 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
-// Eseguire la query per ottenere l'utente con ID 1
-$query = "SELECT * FROM user WHERE ID = 1";
-$result = $mysqli->query($query);
-
-if (!$result) {
-    printf("Errore nella query: %s\n", $mysqli->error);
-    exit();
-}
-
-// Ottenere i dati dell'utente
-$user = $result->fetch_assoc();
-
-// Output dei dati con printf
-printf("ID: %d, username: %s, password: %s", $user['ID'], $user['username'], $user['password']);
-
-// Chiudere la connessione al database
 $mysqli->close();
-exit();
 ?>
